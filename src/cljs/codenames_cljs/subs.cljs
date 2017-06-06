b0090c5adebd01e586a8b468f54805618f5074ac
Michael Fogleman <michaelwfogleman@gmail.com>
2017-06-06 10:29:10 -0400

    Beginning work on showing state.

--- src/cljs/codenames_cljs/subs.cljs
+++ src/cljs/codenames_cljs/subs.cljs
@@ -1,13 +1,20 @@
 (ns codenames-cljs.subs
   (:require-macros [reagent.ratom :refer [reaction]])
-  (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame]
+            [com.rpl.specter :as S]))
+
+(defn cell-filterer [[x y :as target] {:keys [position]}]
+  (= target position))
+
+(defn get-cell-word [game x y]
+  (S/select-any [S/ATOM :game :words (S/filterer #(cell-filterer [x y] %)) S/ALL :word]
+                game))
 
 (re-frame/reg-sub
  :game
- (fn [db]
-   (:game db)))
+ :game)
 
 (re-frame/reg-sub
  :cell
  (fn [db [_ x y]]
-   (reaction (get-in db [:board [x y]]))))
+   [:span (str (get-cell-word db x y))]))
